f232bb8a1d1ad33fd43fa8e2b94869a82a97d01a
NagarajuM1991 <78196976+NagarajuM1991@users.noreply.github.com>
2025-11-19 11:53:27 +0530

    New notes

--- force-app/main/default/lwc/lifeCycleHooks/lifeCycleHooks.js
+++ force-app/main/default/lwc/lifeCycleHooks/lifeCycleHooks.js
@@ -2,27 +2,40 @@ import { LightningElement,wire } from 'lwc';
 import cons from '@salesforce/apex/displayAccountRecords.getAccounts'
 
 export default class LifeCycleHooks extends LightningElement {
-
-    connectedCallback(){
-        console.log('connected callback request')
-        let result = this.consData; 
-    }
+    contacts;
+    error;
 
     constructor(){
         super()
         console.log('constructor callback request')
     }
-
-    contacts;
-    error;
-
+    
     @wire(cons) 
     consData({data,error}){
+        console.log('from wire function',data)
         if(data){
         this.contacts = data;
         }else if(error){
          this.error = error;
         }
     }
+
+    connectedCallback(){
+        console.log('connected callback request')
+        let result = this.consData; 
+    }
+
+    renderedCallback(){
+        console.log('rendered callback request')
+    }
+
+    disconnectedCallback(){
+        console.log('disconnected callback request')
+    }
+
+
+
+    
+
     
 }
